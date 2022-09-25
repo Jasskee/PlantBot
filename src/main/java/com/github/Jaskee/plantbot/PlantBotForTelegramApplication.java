5059f8cb706828e9b3da0400ef934a39a8951bcb
Andrey Voropaev <voropaev.an.99@gmail.com>
2022-09-25 18:52:26 +0300

    PB-0: added SpringBoot skeleton

---
+++ src/main/java/com/github/Jaskee/plantbot/PlantBotForTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.github.Jaskee.plantbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantBotForTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantBotForTelegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Jaskee/plantbot/PlantBotForTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Jaskee.plantbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantBotForTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
